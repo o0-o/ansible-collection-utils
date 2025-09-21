4f78f854bb435c48e3e10a47aca5973e09b11966
oØ.o <o0-o@o0-o.ooo>
2025-09-21 18:47:44 -0400

    Serve generated index as docs root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,9 @@ html_title = "o0_o.utils documentation"
 html_short_title = "o0_o.utils"
 html_baseurl = "https://o0-o.github.io/ansible-collection-utils/"
 
+root_doc = "generated/index"
+master_doc = root_doc
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
@@ -48,4 +51,5 @@ nitpicky = True
 exclude_patterns = [
     "_build",
     "build",
+    "index.rst",
 ]
